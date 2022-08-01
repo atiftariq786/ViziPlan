58dfd72ccc31c357661d30b95d922244a0fd7d42
atiftariq786 <atiftariq786@hotmail.com>
2022-08-01 09:50:11 -0700

    Update API.js

--- myapp/src/services/utils/API.js
+++ myapp/src/services/utils/API.js
@@ -1,62 +1,60 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
+let herokuURL = "https://vizi-plan-server.herokuapp.com/";
+let localHost = "http://localhost:3001/";
+let hostURL = herokuURL;
+
 const API = {
   getVisionBoardImages: (type) => {
-    return axios.get(`http://localhost:3001/api/images/${type}`);
+    return axios.get(`${hostURL}api/images/${type}`);
   },
   addNewImage: (data) => {
-    return axios.post(`http://localhost:3001/api/images/add`, data);
+    return axios.post(`${hostURL}api/images/add`, data);
   },
   saveSelectedImages: (data) => {
-    return axios.post(`http://localhost:3001/api/visionBoard/save`, data);
+    return axios.post(`${hostURL}api/visionBoard/save`, data);
   },
   getSelectedImages: () => {
-    return axios.get(`http://localhost:3001/api/visionBoard/me`);
+    return axios.get(`${hostURL}api/visionBoard/me`);
   },
   deleteSelectedImages: (id) => {
-    return axios.delete(`http://localhost:3001/api/visionBoard/${id}`);
+    return axios.delete(`${hostURL}api/visionBoard/${id}`);
   },
   userLogin: (data) => {
-    return axios.post(`http://localhost:3001/auth/signin`, data);
+    return axios.post(`${hostURL}auth/signin`, data);
   },
   userSignUp: (data) => {
-    return axios.post(`http://localhost:3001/auth/signup`, data);
+    return axios.post(`${hostURL}auth/signup`, data);
   },
   userLogout: () => {
-    return axios.get(`http://localhost:3001/auth/logout`);
+    return axios.get(`${hostURL}auth/logout`);
   },
   isUserLoggedin: () => {
-    return axios.post(`http://localhost:3001/auth/isLoggedIn`);
+    return axios.post(`${hostURL}auth/isLoggedIn`);
   },
   userAddGoal: (data) => {
-    return axios.post(`http://localhost:3001/api/goals/addGoal`, data);
+    return axios.post(`${hostURL}api/goals/addGoal`, data);
   },
   //=========================================
   savedGoal: (type) => {
-    return axios.get(`http://localhost:3001/api/goals/${type}`);
+    return axios.get(`${hostURL}api/goals/${type}`);
   },
   completeGoal: (data) => {
-    return axios.put(
-      `http://localhost:3001/api/goals/completeGoal/${data.id}`,
-      data
-    );
+    return axios.put(`${hostURL}api/goals/completeGoal/${data.id}`, data);
   },
   getActivities: () => {
-    return axios.get(`http://localhost:3001/api/activities`);
+    return axios.get(`${hostURL}api/activities`);
   },
   //==========================================
   deleteGoal: (id) => {
-    return axios.delete(`http://localhost:3001/api/goals/${id}`);
+    return axios.delete(`${hostURL}api/goals/${id}`);
   },
   editUserGoal: (data) => {
-    return axios.put(
-      `http://localhost:3001/api/goals/updateGoal/${data.id}`,
-      data
-    );
+    return axios.put(`${hostURL}api/goals/updateGoal/${data.id}`, data);
   },
   allGoalsCreatedAnalytics: () => {
-    return axios.get(`http://localhost:3001/api/analytics/allGoalsCreated`);
+    return axios.get(`${hostURL}api/analytics/allGoalsCreated`);
   },
 };
 
